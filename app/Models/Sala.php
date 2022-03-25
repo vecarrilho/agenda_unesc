ed2b4df9200801f406a54e24d16cf6cdf68b81fb
Vitor Elias Ferreira Carrilho <vecarrilho@unesc.net>
2022-03-25 16:52:03 -0300

    Criação de scope de data e hora

--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -10,5 +10,14 @@ class Sala extends Model
     public function scopeExibicao($query){
         return $query->where('data', '>=', date('Y-m-d'));
     }
+
+    public function scopeData($query, $data){
+        return $query->where('data', $data);
+    }
+
+    public function scopeHora($query, $hora){
+        return $query->where('hora', date('H:i:s', strtotime($hora)));
+    }
+
     use HasFactory;
 }
