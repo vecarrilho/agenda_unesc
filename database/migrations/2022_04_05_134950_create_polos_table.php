558e9a8b7a482d1d1229af33099d3cbbc2de547d
Vitor Elias Ferreira Carrilho <vecarrilho@unesc.net>
2022-04-05 14:12:03 -0300

    Criação da entidade dos polos

---
+++ database/migrations/2022_04_05_134950_create_polos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePolosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao', 40);
+            $table->string('endereco', 100);
+            $table->string('localizacao');
+            $table->string('status', 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polos');
+    }
+}
