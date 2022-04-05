0520e9711dd879cb0212d0212a77c1e14fe9a58f
Vitor Elias Ferreira Carrilho <vecarrilho@unesc.net>
2022-04-05 10:11:30 -0300

    Adicionado inserção do polo no insert

---
+++ database/migrations/2022_04_01_200241_insert_salas_data.php
@@ -0,0 +1,54 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class InsertSalasData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('salas')->insert(
+            array(
+                'bloco' => 'XXIA',
+                'hora' => '19:00:00',
+                'data' => '2022-04-30',
+                'qtd_maquinas' => 20,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'nsala' => 1,
+                'polo' => 'POLO CRICIMA'
+            )
+        );
+        
+        DB::table('salas')->insert(
+            array(
+                'bloco' => 'XXIB',
+                'hora' => '12:00:00',
+                'data' => '2022-04-20',
+                'qtd_maquinas' => 30,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'nsala' => 2,
+                'polo' => 'POLO CRICIMA'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
