958dbfa20f06b188792380ed2d1ba545a6b66169
Vitor Elias Ferreira Carrilho <vecarrilho@unesc.net>
2022-04-08 07:59:53 -0300

    Removido botão de Meus Agendamentos

--- resources/views/agenda/myList.blade.php
+++ resources/views/agenda/myList.blade.php
@@ -45,8 +45,7 @@
     </div>
     <div class="container">
         <ul class="lista-botoes inline-flex">
-            <li><a href="{{ route('agenda.show', true) }}" class="btn btn-primary">Agendamentos Disponíveis</a></li>
-            <li><a href="{{ route('agenda.myList', Auth::user()->id) }}" class="btn btn-primary">Meus Agendamentos</a></li>
+            <li><a href="{{ route('agenda.show', true) }}" class="btn btn-primary">Horários Disponíveis</a></li>
         </ul>
         <h2>Meus horários agendados</h2>
         <table class="table">
